0c3981cb27a2485b5ce41a0ae7034bf78a22e082
Anton Yemelyanov <anton.yemelyanov@gmail.com>
2014-06-25 19:41:59 -0400

    Better handling of failures and deprecated use. zetta-base58 is now a standard that will be replacing bs58 dependencies.

--- zetta-base58.js
+++ zetta-base58.js
@@ -5,18 +5,23 @@ base58.decodeSync = base58.decode;
 
 base58.encode = function(input, callback) {
 	var result = base58.encodeSync(input);
-	callback(null, result);
+	callback && callback(null, result);
+	return result;
 }
 
 base58.decode = function(input, callback) {
+	if(!callback)
+		throw new Error("decode(input, callback) requires callback argument (old sync version deprecated)");
 
+	var result = null;
 	try {
-		var result = base58.decodeSync(input);
-		callback(null, result);
+		result = base58.decodeSync(input);
 	} catch(ex) {
+		console.log(ex);
 		return callback({ error : ex });
 	}
 
+	callback(null, result);
 }
 
 module.exports = base58;
